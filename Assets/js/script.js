24f5ab62691cb8846f327d6cff5402ee0830525c
Maribel Montes <maribel.doris@gmail.com>
2022-01-13 09:32:14 -0600

    checking the functionality of the eventlistener

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,7 +2,6 @@
 
 var currentDayEl = $('#currentDay');
 
-
 // day display on the header id="currentDay" class="lead"
 function displayDay(){
     var today = moment().format('dddd, MMMM Do');
@@ -27,19 +26,48 @@ var saveElem2pm = $('.bt2-pm');
 var saveElem3pm = $('.bt3-pm');
 var saveElem4pm = $('.bt4-pm');
 
-saveElem9am.on("click", saveMSG(1));
-saveElem10am.on("click", saveMSG(2));
-saveElem11am.on("click", saveMSG(3));
-saveElem12pm.on("click", saveMSG(4));
-saveElem1pm.on("click", saveMSG(5));
-saveElem2pm.on("click", saveMSG(6));
-saveElem3pm.on("click", saveMSG(7));
-saveElem4pm.on("click", saveMSG(8));
+var displayhour9am = $('#hour9-am');
+var displayhour10am = $('#hour10-am');
+var displayhour11am = $('#hour11-am');
+var displayhour12pm = $('#hour12-pm');
+var displayhour1pm = $('#hour1-pm');
+var displayhour2pm = $('#hour2-pm');
+var displayhour3pm = $('#hour3-pm');
+var displayhour4pm = $('#hour4-pm');
+
+saveElem9am.on('click', saveMSG);
+// saveElem10am.on('click', saveMSG(2));
+// saveElem11am.on('click', saveMSG(3));
+// saveElem12pm.on('click', saveMSG(4));
+// saveElem1pm.on('click', saveMSG(5));
+// saveElem2pm.on('click', saveMSG(6));
+// saveElem3pm.on('click', saveMSG(7));
+// saveElem4pm.on('click', saveMSG(8));
 
 // 9am = 1, 10am = 2, 11am = 3, 12pm = 4; 1pm = 5
 // 2pm = 6, 3pm = 7, 4pm = 8
-function saveMSG(timeofDay){
-    
-    if
+function saveMSG(event){
+    event.preventDefault();
+    // event.preventDefault();
+
+    var timeofDay = 1;
+
+    // if (timeofDay == 1){
+    var hour = displayhour9am.value;
+console.log(displayhour9am.text);
+    localStorage.setItem("hour9am", hour);
+    // }
 
 }
+
+// var userPasswordSpan = document.querySelector("#user-password");
+
+// function initLocal (){
+//     var hour9am = localStorage.getItem("hour9am");
+
+//     displayhour9am.textContent = hour9am;
+
+
+// }
+
+// initLocal ();
