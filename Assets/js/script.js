fccff0b9fe4a8b77631755306f974ee7bcbd650d
Maribel Montes <maribel.doris@gmail.com>
2022-01-14 22:59:43 -0600

    added code to pull message from display

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -25,6 +25,7 @@ var saveElem1pm = $('.bt1-pm');
 var saveElem2pm = $('.bt2-pm');
 var saveElem3pm = $('.bt3-pm');
 var saveElem4pm = $('.bt4-pm');
+var saveElem5pm = $('.bt5-pm');
 
 var displayhour9am = $('#hour9-am');
 var displayhour10am = $('#hour10-am');
@@ -34,40 +35,111 @@ var displayhour1pm = $('#hour1-pm');
 var displayhour2pm = $('#hour2-pm');
 var displayhour3pm = $('#hour3-pm');
 var displayhour4pm = $('#hour4-pm');
-
-saveElem9am.on('click', saveMSG);
-// saveElem10am.on('click', saveMSG(2));
-// saveElem11am.on('click', saveMSG(3));
-// saveElem12pm.on('click', saveMSG(4));
-// saveElem1pm.on('click', saveMSG(5));
-// saveElem2pm.on('click', saveMSG(6));
-// saveElem3pm.on('click', saveMSG(7));
-// saveElem4pm.on('click', saveMSG(8));
+var displayhour5pm = $('#hour5-pm');
 
 // 9am = 1, 10am = 2, 11am = 3, 12pm = 4; 1pm = 5
-// 2pm = 6, 3pm = 7, 4pm = 8
-function saveMSG(event){
+// 2pm = 6, 3pm = 7, 4pm = 8, 5pm = 9
+
+// button 9am
+saveElem9am.on('click', function(event){
     event.preventDefault();
-    // event.preventDefault();
+    var message = displayhour9am.val();
+    localStorage.setItem("hour9am", message);
+});
 
-    var timeofDay = 1;
+// button 10am
+saveElem10am.on('click', function(event){
+    event.preventDefault();
+    var message = displayhour10am.val();
+    localStorage.setItem("hour10am", message);
+});
 
-    // if (timeofDay == 1){
-    var hour = displayhour9am.value;
-console.log(displayhour9am.text);
-    localStorage.setItem("hour9am", hour);
-    // }
+// button 11am
+saveElem11am.on('click', function(event){
+    event.preventDefault();
+    var message = displayhour11am.val();
+    localStorage.setItem("hour11am", message);
+});
 
+// button 12pm
+saveElem12pm.on('click', function(event){
+    event.preventDefault();
+    var message = displayhour12pm.val();
+    localStorage.setItem("hour12pm", message);
+});
+
+// button 1pm
+saveElem1pm.on('click', function(event){
+    event.preventDefault();
+    var message = displayhour1pm.val();
+    localStorage.setItem("hour1pm", message);
+});
+
+// button 2pm
+saveElem2pm.on('click', function(event){
+    event.preventDefault();
+    var message = displayhour2pm.val();
+    localStorage.setItem("hour2pm", message);
+});
+
+// button 3pm
+saveElem3pm.on('click', function(event){
+    event.preventDefault();
+    var message = displayhour3pm.val();
+    localStorage.setItem("hour3pm", message);
+});
+
+// button 4pm
+saveElem4pm.on('click', function(event){
+    event.preventDefault();
+    var message = displayhour4pm.val();
+    localStorage.setItem("hour4pm", message);
+});
+
+// button 5pm
+saveElem5pm.on('click', function(event){
+    event.preventDefault();
+    var message = displayhour5pm.val();
+    localStorage.setItem("hour5pm", message);
+});
+    
+
+function initLocal (){
+    // save the 9am message at start of page
+    var hourofDay = localStorage.getItem("hour9am");
+    document.getElementById("hour9-am").innerHTML = hourofDay;
+
+    // save the 9am message at start of page
+    var hourofDay = localStorage.getItem("hour9am");
+    document.getElementById("hour9-am").innerHTML = hourofDay;
+
+    // save the 9am message at start of page
+    var hourofDay = localStorage.getItem("hour9am");
+    document.getElementById("hour9-am").innerHTML = hourofDay;
+
+    // save the 9am message at start of page
+    hourofDay = localStorage.getItem("hour12pm");
+    document.getElementById("hour12-pm").innerHTML = hourofDay;
+
+    // save the 9am message at start of page
+    hourofDay = localStorage.getItem("hour1pm");
+    document.getElementById("hour1-pm").innerHTML = hourofDay;
+
+    // save the 9am message at start of page
+    hourofDay = localStorage.getItem("hour2pm");
+    document.getElementById("hour2-pm").innerHTML = hourofDay;
+
+    // save the 9am message at start of page
+    hourofDay = localStorage.getItem("hour3pm");
+    document.getElementById("hour3-pm").innerHTML = hourofDay;
+
+    // save the 9am message at start of page
+    hourofDay = localStorage.getItem("hour4pm");
+    document.getElementById("hour4-pm").innerHTML = hourofDay;
+
+    // save the 5pm message at start of page
+    hourofDay = localStorage.getItem("hour5pm");
+    document.getElementById("hour5-pm").innerHTML = hourofDay;
 }
 
-// var userPasswordSpan = document.querySelector("#user-password");
-
-// function initLocal (){
-//     var hour9am = localStorage.getItem("hour9am");
-
-//     displayhour9am.textContent = hour9am;
-
-
-// }
-
-// initLocal ();
+initLocal ();
